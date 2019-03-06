6feecfcf966315312e8aaf64bcb5484eee9afb0e
Rafael Paz <rafaelpaz86@gmail.com>
2019-03-06 14:03:33 -0300

    I need the Button save to finish the register process.

--- app/src/main/java/br/com/alura/mercadoonline/CadastroActivity.java
+++ app/src/main/java/br/com/alura/mercadoonline/CadastroActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.mercadoonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Button saveButton = (Button) findViewById(R.id.cadastroSalvar);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
