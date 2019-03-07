2a9ed202e37e4da0b8bee78dbe38c007dcaea556
Rafael Paz <rafaelpaz86@gmail.com>
2019-03-07 10:38:40 -0300

    It was added interation between the FirstActivity and CadastroActivity

--- app/src/main/java/br/com/alura/mercadoonline/FirstActivity.java
+++ app/src/main/java/br/com/alura/mercadoonline/FirstActivity.java
@@ -1,7 +1,10 @@
 package br.com.alura.mercadoonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Button primeiroAcesso = (Button) findViewById(R.id.primeiroAcesso);
+        primeiroAcesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vaiProCadastro = new Intent(FirstActivity.this, CadastroActivity.class);
+                startActivity(vaiProCadastro);
+
+            }
+        });
+
     }
 }
