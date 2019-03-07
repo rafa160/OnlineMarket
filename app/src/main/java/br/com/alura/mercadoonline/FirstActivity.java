9d9f17ebf749fbb4f47eee0f8de663437b45f52d
Rafael Paz <rafaelpaz86@gmail.com>
2019-03-07 10:58:51 -0300

    I just added the motion it says 'Blanked spaces' in login area, added as well the new MercadoActivity

--- app/src/main/java/br/com/alura/mercadoonline/FirstActivity.java
+++ app/src/main/java/br/com/alura/mercadoonline/FirstActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -19,6 +20,26 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent vaiProCadastro = new Intent(FirstActivity.this, CadastroActivity.class);
                 startActivity(vaiProCadastro);
+
+            }
+        });
+
+        Button mercadoAcesso = (Button) findViewById(R.id.buttomLogin);
+        mercadoAcesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText login = (EditText) findViewById(R.id.login);
+                EditText password = (EditText) findViewById(R.id.password);
+
+                if(login.getText().toString().length() == 0){
+                    login.setError("Campo em Branco");
+                }if(password.getText().toString().length() == 0){
+                    password.setError("Campo em Branco");
+                } else{
+                    Intent vaiProMercado = new Intent(FirstActivity.this, MercadoActivity.class);
+                    startActivity(vaiProMercado);
+                }
 
             }
         });
